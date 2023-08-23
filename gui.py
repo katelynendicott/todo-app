19b92c41595748c37da2a487ca75396048459c8e
Katelyn <katelyn.endicott@gmail.com>
2023-08-22 22:00:02 -0700

    Create basic gui for todo app without interactions. Day 16

--- gui.py
+++ gui.py
@@ -1,1 +1,10 @@
 import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Enter a todo: ")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('ToDo App', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
